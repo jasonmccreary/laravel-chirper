426296fb95f547fc4c267bc374a2e6ddfd7bf882
Jason McCreary <jason@pureconcepts.net>
2023-05-12 12:49:20 -0400

    Showing chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -12,4 +12,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
