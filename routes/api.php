6192e0bfb70d015b52c9f30686de95b7606174f9
Jason McCreary <jason@pureconcepts.net>
2023-05-14 09:24:26 -0400

    Simple API endpoints for testing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/status', \App\Http\Controllers\Api\HealthCheck::class);
+Route::post('/loopback', \App\Http\Controllers\Api\Loopback::class);
+Route::apiResource('/chirps', \App\Http\Controllers\Api\ChirpController::class)->only('index', 'show');
