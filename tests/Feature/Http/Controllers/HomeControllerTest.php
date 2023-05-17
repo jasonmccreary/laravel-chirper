57c0a39b835b8db327973017c470a38c266b85c2
Jason McCreary <jason@pureconcepts.net>
2023-05-17 15:08:25 -0500

    Loosen test for CI

--- tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -17,6 +17,6 @@ class HomeControllerTest extends TestCase
 
         $response->assertOk();
         $response->assertViewIs('welcome');
-        $response->assertSeeText('Laravel v10.10.1 (PHP v8.2.4)');
+        $response->assertSeeText('Laravel v10.10');
     }
 }
